66d75d91ae83771c30ccafab037052dd70368408
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-02-22 11:38:15 +0100

    counter wasnt resetting properly without the casting, very weird

--- core/src/mytask.c
+++ core/src/mytask.c
@@ -5,6 +5,7 @@ const int period = 1000; // thread period in ticks
 
 unsigned long int* pt_counter = NULL;
 unsigned long int counter = 0 ;
+int flag = 0;
 
 void print (void *uart){
     char msg[36];
@@ -17,24 +18,29 @@ void print (void *uart){
         // vTaskDelayUntil(&lastWakeTime, period);
         vTaskDelay(period);
 
-        if (pt_counter) {
+        if (flag) {
             sprintf(msg, "Counter in HEAP  Iterations: %ld \n\r", *pt_counter);
-            *pt_counter = 0;
         } else {
             sprintf(msg, "Counter in STACK Iterations: %ld \n\r", counter);
-            counter = 0;
         }
-        
         // print message to serial
         HAL_UART_Transmit(uart, (uint8_t *)msg, strlen(msg), 0xFFFF);
+
         // toggle led
         HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+
+        //reset counters (done here for more "fair" comparison)
+        pt_counter = (unsigned long int *) pvPortMalloc(sizeof(int));
+        // find it really weird that this casting is needed
+        //but otherwise the counter is not reset
+        counter = (unsigned long int) 0; 
     }
 }
 
 /* increment counter stored in HEAP with malloc */
 void IncrementCounterPointer(){
 
+    flag = 1;
     pt_counter = (unsigned long int *) pvPortMalloc(sizeof(int));
 
     while (1) {
