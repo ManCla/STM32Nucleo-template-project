d1b779fd6b3926ffc6801cde61025f59282eb213
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-02 10:39:25 +0100

    general improvements

--- core/src/mytask.c
+++ core/src/mytask.c
@@ -2,17 +2,19 @@
     sprintf(err_msg, x);        \
     HAL_UART_Transmit(uart, (void*)err_msg,strlen(err_msg), 0xFFFF);
 
+#define BUFFER_LENGTH 64
+
 #include <string.h>
 #include <stdio.h>
 
 const int period = 1000;          // thread period in ticks
-QueueHandle_t readingQueue;       // queue containing incoming bytes
+QueueHandle_t inQueue;       // queue containing incoming bytes
 SemaphoreHandle_t printSemaphore; // semaphore to signal printing
 
 void server (void *uart){
     
     // create input queue and printing semaphore
-    readingQueue   = xQueueCreate( 64, sizeof( char ) );
+    inQueue   = xQueueCreate( BUFFER_LENGTH, sizeof( char ) );
     printSemaphore = xSemaphoreCreateBinary();
 
     // assuming creation of queue and semaphore was successful
@@ -28,7 +30,7 @@ void server (void *uart){
         char read;
         do {
             HAL_UART_Receive(uart, (void*)&read, 1, 0xFFFF);
-            if(xQueueSendToBack(readingQueue, (void *)&read, 0) != pdPASS){
+            if(xQueueSendToBack(inQueue, (void *)&read, 0) != pdPASS){
                 DEBUG_MESSAGE("\n\r ### WARNING: input queue is full ### \n\r")
                 break;
             }
@@ -45,18 +47,16 @@ void server (void *uart){
 
 
 void printer (void *uart){
-    char msg;
-
+    char msg[BUFFER_LENGTH+1]; // make sure that there is space for newline char
+    int i=0;
     while ( 1 ) {
 
         if ( xSemaphoreTake(printSemaphore, (TickType_t) 100 ) == pdTRUE ) {
+            i=0;
+            while (xQueueReceive(inQueue,msg+i,0)==pdPASS && i<BUFFER_LENGTH) ++i;
+            msg[i]='\r';
+            HAL_UART_Transmit(uart, (void*)msg, i, 0xFFFF);
             
-            while (xQueueReceive(readingQueue,&msg,0) == pdPASS){
-                HAL_UART_Transmit(uart, (void*)&msg, 1, 0xFFFF);
-            }
-            msg='\n\r';
-            HAL_UART_Transmit(uart, (void*)&msg, 1, 0xFFFF);
-            xQueueReset(readingQueue);
         }
     }
 }
