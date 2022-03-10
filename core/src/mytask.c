b5fc70197cff3e9d633c2ba033f72a387e1028c7
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-10 10:53:38 +0100

    polishing exercise

--- core/src/mytask.c
+++ core/src/mytask.c
@@ -3,19 +3,19 @@
 
 const int period = 1000; // thread period in ticks
 int adc_value;
+int counter=0;
 
 void hello (void *uart){
     
-    TickType_t lastWakeTime = xTaskGetTickCount();
-        
     while (1){
         // timing code
-        vTaskDelayUntil(&lastWakeTime, period);
+        vTaskDelay(period);
         
         // count periods
         char msg[36];
-        sprintf(msg, "-%d-\n\r", adc_value);
+        sprintf(msg, "Value: %d -- Number of samples: %d \n\r", adc_value, counter);
         HAL_UART_Transmit(uart, (uint8_t *)msg, strlen(msg), 0xFFFF);
+        counter=0;
 
         //toggle led
         HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
@@ -25,19 +25,13 @@ void hello (void *uart){
 
 void adc_read (void *adc){
     
-    TickType_t lastWakeTime = xTaskGetTickCount();
-        
     while (1){
-        // timing code
-        vTaskDelayUntil(&lastWakeTime, period);
-        
+    
         HAL_ADC_Start(adc); 
         HAL_ADC_PollForConversion(adc, 1);
         adc_value = HAL_ADC_GetValue(adc);
         HAL_ADC_Stop(adc);
+        counter++;
 
-
-        //toggle led
-        HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
     }
 }
